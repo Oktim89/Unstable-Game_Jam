5b4fce74f2590bad8b74b04731854568d9a6ee21
Ju-im <111383569+Oktim89@users.noreply.github.com>
2026-08-16 20:05:19 +0100

    finfished the dragging

---
+++ unstable-project/Item.gd
@@ -0,0 +1,31 @@
+extends Node2D
+var in_area
+var follow= false
+var is_picked_up:bool = false
+
+
+func _input(event: InputEvent) -> void:
+
+		if event is InputEventMouseButton:
+				if in_area and event.button_index==1 and event.pressed:
+					self.freeze= true
+					follow=true
+				if event.is_released():
+					follow=false
+					self.freeze=false
+					
+func _process(delta: float) -> void:
+	if follow:
+		position = get_global_mouse_position()
+	
+		
+		
+
+
+func _on_area_2d_mouse_entered() -> void:
+	in_area=true
+	
+
+
+func _on_area_2d_mouse_exited() -> void:
+	in_area=false
