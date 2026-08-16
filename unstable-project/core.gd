638104db6e88e9cd177b3e57d31a43adfc5c0744
Ju-im <111383569+Oktim89@users.noreply.github.com>
2026-08-16 21:04:54 +0100

    update

--- unstable-project/core.gd
+++ unstable-project/core.gd
@@ -2,8 +2,14 @@ extends RigidBody2D
 var in_mouse_area=false
 var follow= false
 var connected= false
-var parent = null
-var in_conneted_area=false
+var parent:Node = null
+var in_connected_area
+var  level_parent:Node 
+
+
+func _ready() -> void:
+	level_parent =get_parent()
+	
 func _input(event: InputEvent) -> void:
 
 		if event is InputEventMouseButton:
@@ -13,18 +19,25 @@ func _input(event: InputEvent) -> void:
 				if event.is_released():
 					follow=false
 					self.freeze=false
-					switch_parent()
+					attach_to_parent()
 					
-func switch_parent():
-	if !in_conneted_area:
+func attach_to_parent():
+	if !in_connected_area:
+		reparent(level_parent)
+		return
+	if parent==null:
+		reparent(level_parent)
 		return
 	print(parent)
+	self.reparent(parent,true)
+	connected=true
+	parent=null
 func _process(delta: float) -> void:
 	if follow:
 		position = get_global_mouse_position()
 		
 func _on_edge_area_entered(area: Area2D) -> void:
-	in_conneted_area=true
+	in_connected_area=true
 	if follow:
 		parent=area.get_parent()
 	
@@ -36,8 +49,8 @@ func _on_mouse_area_mouse_entered() -> void:
 
 func _on_mouse_area_mouse_exited() -> void:
 	in_mouse_area=false
-	print("not in area")
+	
 
 
 func _on_edge_area_exited(area: Area2D) -> void:
-	in_conneted_area=false
+	in_connected_area=false
